2cd938cd756b3737c457f85e4e1764970f19fe11
daudujohn <daudujoe@gmail.com>
2023-06-15 05:11:11 -0700

    initial commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,10 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main()
 {
-	FILE *stderr = stderr;
-fprintf(
-stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	FILE *stderr = stderr
+fprintf
+	(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
 			}
