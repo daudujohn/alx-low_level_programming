c3f473482ecd2d8ad9297ce570eefcfa4135c02c
daudujohn <daudujoe@gmail.com>
2023-06-20 09:43:03 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+using namespace std;
+
+/**
+ * main - see code
+ *
+ * Return: sum.
+ */
+
+int main(void)
+{
+int i = 0;
+int sum = 0;
+for (i = 0; i < 1024; i++)
+{
+if((i % 3) == 0 || (i % 5) == 0))
+{
+sum = sum + i;
+printf("%d\n", i);
+}
+}
+return sum;
+}
