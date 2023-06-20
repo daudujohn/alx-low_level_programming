d0b0768494e8945b76d15ff1f44ccbca23f1a455
daudujohn <daudujoe@gmail.com>
2023-06-20 06:27:50 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+for(i = 97; i < 123;  i++)
+	_putchar("%c ", i);
+_putchar("\n");
+return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,2 @@
 void *_putchar(char);
+void print_alphabet_x10(void);
