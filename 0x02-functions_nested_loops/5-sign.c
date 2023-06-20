6005e5f77de0620e28216d476624af65b1a38b02
daudujohn <daudujoe@gmail.com>
2023-06-20 08:21:15 -0700

    initial commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,17 +12,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-_putchars('+');
+_putchar('+');
 return (1);
 }
 else if (n == 0)
 {
-_putchars('0');
+_putchar('0');
 return (0);
 }
 else
 {
-_putchars('-');
+_putchar('-');
 return (-1);
 }
 }
