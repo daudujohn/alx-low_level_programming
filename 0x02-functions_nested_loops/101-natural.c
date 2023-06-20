dfc4dceb2abb6e405265b1dc259f817cc6541a23
daudujohn <daudujoe@gmail.com>
2023-06-20 09:51:27 -0700

    initial commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,11 +13,11 @@ int i = 0;
 int sum = 0;
 for (i = 0; i < 1024; i++)
 {
-if((i % 3) == 0 || (i % 5) == 0)
+if ((i % 3) == 0 || (i % 5) == 0)
 {
 sum = sum + i;
 }
 }
 printf("%d\n", sum);
-return sum;
+return (sum);
 }
