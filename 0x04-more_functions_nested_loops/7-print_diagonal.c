3022bbeaa871fc544890217d3d75dd088d14cd5e
daudujohn <daudujoe@gmail.com>
2023-06-22 07:08:10 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - check the code
+ * @n: number of times to print \
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('\');
+_putchar('\n');
+}
+putchar('\n')'
+}
