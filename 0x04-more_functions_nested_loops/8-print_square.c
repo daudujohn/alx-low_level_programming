830a3c06f6a2403aafbd73c9a009c5c116efadae
daudujohn <daudujoe@gmail.com>
2023-06-22 07:16:02 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - check the code
+ * @size: number of times to print #
+ * Return: void
+ */
+
+void print_square(int size)
+{
+int i;
+for (i = 0; i < size; i++)
+{
+_putchar('#');
+}
+_putchar('\n')'
+}
