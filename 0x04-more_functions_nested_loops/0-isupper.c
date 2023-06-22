5d5d46f6b442e68a514f459da7435190d63fc3e0
daudujohn <daudujoe@gmail.com>
2023-06-22 05:37:19 -0700

    initial commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,7 +10,6 @@
 
 int _isupper(int c)
 {
-char c = (char)c;
 if (c >= 'A' || c <= 'Z')
 {
 return (1);
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul- check the code
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
