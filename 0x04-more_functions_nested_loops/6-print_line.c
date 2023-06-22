821794b0c960a584dd950ddeceb718dd7b2c1926
daudujohn <daudujoe@gmail.com>
2023-06-22 06:51:19 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - check the code
+ * @n: number of times to print _
+ * Return: void
+ */
+
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n')'
+}
