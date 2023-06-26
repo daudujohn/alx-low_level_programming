16e268b704cfeca5f39b426b95caff55427016de
daudujohn <daudujoe@gmail.com>
2023-06-26 04:09:18 -0700

    initial commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,12 +10,13 @@
 int main(void)
 {
 int length = 8;
-char password[length];
+char *password = malloc(length * sizeof(char));
 int character;
+int i;
 
 srand(time(NULL));
 
-for (int i = 0; i < length; i++)
+for (i = 0; i < length; i++)
 {
 character = rand() % 94 + 33;
 password[i] = character;
