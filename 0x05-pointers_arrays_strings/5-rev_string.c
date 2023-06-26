4282cf3256c91948c57b91b11aa13bea7b1e2743
daudujohn <daudujoe@gmail.com>
2023-06-26 03:30:20 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - check the code
+ * @s: string to be reversed
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+int length = sizeof(s);
+int i = 0;
+int j = length - 1;
+
+while (i < j)
+{
+char temp = s[i];
+s[i] = s[j];
+s[j] = temp;
+i++;
+j--;
+}
+}
