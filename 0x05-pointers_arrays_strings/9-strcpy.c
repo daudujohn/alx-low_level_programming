49ec4a935e7175f7b4b92d08ad5e8834e6b8ec19
daudujohn <daudujoe@gmail.com>
2023-06-26 03:58:10 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - check the code
+ * @dest: copy destination
+ * @src: copy source
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+char *p = dest;
+
+while (*src != '\0')
+{
+*p++ = *src++;
+}
+
+*p = '\0';
+
+return dest;
+}
