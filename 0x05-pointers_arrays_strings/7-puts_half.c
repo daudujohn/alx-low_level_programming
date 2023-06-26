22c91b455453d1531b2dde9ea207bc747769a9f4
daudujohn <daudujoe@gmail.com>
2023-06-26 03:45:43 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - check the code
+ * @str: string to be printed
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+int length = _strlen(str);
+int half = length / 2;
+if (length % 2 == 1)
+{
+half += 1;
+}
+
+for (int i = half; i < length; i++)
+{
+_putchar(str[i]);
+}
+
+_putchar('\n');
+}
