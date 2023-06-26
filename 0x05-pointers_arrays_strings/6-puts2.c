643e383e3663c2dae1cf1d79ac75b14c91e36566
daudujohn <daudujoe@gmail.com>
2023-06-26 03:40:01 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - check the code
+ * @str: string to be printed
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+int i;
+while (str != '\0')
+{
+_putchar(str[i]);
+i = i + 2;
+}
+_putchar('\n');
+}
