df06be852f6318bcfc27d0d7a1b86f8401ab6776
daudujohn <daudujoe@gmail.com>
2023-06-26 03:08:24 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - check the code
+ * @str: string to be printed
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+s++;
+}
+}
