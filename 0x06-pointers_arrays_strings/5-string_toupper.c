b049951268f266e67cb092200992fb2e8ac561da
daudujohn <daudujoe@gmail.com>
2023-06-29 11:43:07 -0700

    initial commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * string_toupper - see code
+ * @s: string to be changed to uppercase
+ * Return: char
+ */
+
+char *string_toupper(char *s)
+{
+int i;
+i = 0;
+while(s[i] != '\0')
+{
+s[i] = *s[i] - 32;
+}
+return (s);
+}
