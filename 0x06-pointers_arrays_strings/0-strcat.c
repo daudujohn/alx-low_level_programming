71acee8623561a640ff5c83ef3108356ff55043d
daudujohn <daudujoe@gmail.com>
2023-06-29 09:52:53 -0700

    initial commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,11 +14,11 @@ char *_strcat(char *dest, char *src)
 int dest_len, src_len;
 dest_len = 0;
 src_len = 0;
-while (dest[dest_len])
+while (dest[dest_len] != '\0')
 {
 dest_len++;
 }
-while (src[src_len])
+while (src[src_len] != '\0')
 {
 dest[dest_len] = src[src_len];
 dest_len++;
