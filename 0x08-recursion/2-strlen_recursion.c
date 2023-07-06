5f890909bbdb42caaf6444e6111675747f8d81ac
daudujohn <daudujoe@gmail.com>
2023-07-06 09:13:02 -0700

    initial commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,9 @@
  */
 int _strlen_recursion(char *s)
 {
-int i;
-i = 0;
 if (*s != '\0')
 {
-i = i + 1;
-return (_strlen_recursion(s + 1));
+return (1 + _strlen_recursion(s + 1));
 }
-return (i);
+return (0);
 }
