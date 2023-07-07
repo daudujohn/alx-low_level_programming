a01def6bc85316661983b19ece59e38b5718e771
daudujohn <daudujoe@gmail.com>
2023-07-07 08:24:11 -0700

    initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <main.h>
+
+using namespace std;
+/*
+ * print_program_name - prints the name of a program
+ *
+ * Returns: void
+ * /
+ 
+void print_program_name(void)
+{
+cout << argv[0] << endl;
+}
