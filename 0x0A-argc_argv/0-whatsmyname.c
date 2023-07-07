8292fb822b7e904b3d0a36e3f864ec091c652a33
daudujohn <daudujoe@gmail.com>
2023-07-07 14:49:21 -0700

    initial commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/*
+/**
  * main - prints the name of a program
  * 
  * @argc: number of parameters
  * @argv: pointer to an array of pointers
  * Return: 0
- * /
+ */
  
 int main(int argc, char **argv)
 {
