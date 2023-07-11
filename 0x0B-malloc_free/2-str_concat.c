bc7111651a62c9cee97fd0c588cc268612a5b23d
daudujohn <daudujoe@gmail.com>
2023-07-11 08:40:20 -0700

    initial commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - see code
+ * @s1: first string
+ * @s2: second string
+ * 
+ * Return: char
+ */
+
+char *str_concat(char *s1, char *s2) {
+int i;
+int j;
+int length1;
+int length2;
+int total_length;
+char *new_str;
+if (s1 == NULL)
+{
+s1 = '';
+}
+if (s2 == NULL)
+{
+s2 = '';
+}
+
+length1 = strlen(s1);
+length2 = strlen(s2);
+total_length = length1 + length2 + 1; // +1 for the null terminator
+
+new_str = malloc(total_length * sizeof(char));
+if (new_str == NULL)
+{
+return (NULL);
+}
+for (int i = 0; i < length1; i++)
+{
+new_str[i] = s1[i];
+}
+
+for (i = 0; i < length2; i++)
+{
+new_str[i + length1] = s2[i];
+}
+new_str[total_length - 1] = '\0';
+
+return (new_str);
+}
