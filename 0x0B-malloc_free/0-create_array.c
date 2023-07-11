690c37b1099f85d71dedc36531ca613ffec7626a
daudujohn <daudujoe@gmail.com>
2023-07-11 08:19:13 -0700

    initial commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,12 +10,12 @@
 
 char *create_array(unsigned int size, char c)
 {
+int i;
+char *arr;
 if (size == 0)
 {
 return (NULL);
 }
-int i;
-char *arr;
 arr = malloc(size * sizeof(char));
 if (arr != NULL)
 {
