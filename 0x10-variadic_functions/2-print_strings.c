bfd650f3f2f413f28a47520e5ca62926d95b4f62
daudujohn <daudujoe@gmail.com>
2023-07-26 11:54:12 -0700

    initial commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,50 +1,33 @@
-#include "main.h"
+#include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
-/**
- * print_strings - see code
- * @separator: String that separates strings
- * @n: number of variadic argument
- * @...: varadic argument
- *
- * Return: void
- */
-void print_strings(const char *separator, const unsigned int n, ...)
-{
-unsigned int i;
-va_list list;
-char *tmp;
 
-if (n == 0)
+void print_strings(const char *separator, const unsigned int n, ...) {
+unsigned int i;
+va_list args;
+
+va_start(args, n);
+for (i = 0; i < n; i++)
 {
-printf("\n");
-return;
-}
-va_start(list, n);
-for (i = 0; i < n - 1; i++)
+char *str = va_arg(args, char *);
+
+if (str)
 {
-tmp = va_arg(list, char *);
-if (tmp)
-{
-printf("%s", tmp);
+printf("%s", s);
 }
 else
 {
-printf("(nil)");
+printf("NULL");
 }
-if (separator)
+
+if (separator != NULL)
+{
+if ( && i < (n - 1))
 {
 printf("%s", separator);
 }
 }
-tmp = va_arg(list, char *);
-if (tmp)
-{
-printf("%s\n", tmp);
 }
-else
-{
-printf("(nil)\n");
-}
-va_end(list);
+printf("\n");
+va_end(args);
 }
