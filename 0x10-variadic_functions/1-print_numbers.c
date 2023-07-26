6c89f4ff8ce70a304d0f34c3dabb71ad03d9b5f4
daudujohn <daudujoe@gmail.com>
2023-07-26 11:36:40 -0700

    initial commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,14 +12,13 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 unsigned int i;
-char *str;
 
 va_list args;
 va_start(args, n);
 
 for (i = 0; i < n; i++)
 {
-str = va_arg(args, char *);
+char *str = va_arg(args, char *);
 
 if (str)
 {
